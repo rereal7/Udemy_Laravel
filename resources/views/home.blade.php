c04737b30f6d574b6c9214fda2acb9add5819e56
Takuya Kogawa <tk.ngskgtc.vt@gmail.com>
2021-05-26 14:43:22 +0900

    update home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">お知らせ</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,8 +14,10 @@
                         </div>
                     @endif
 
-                    You are logged in!
-                </div>
+                    ログインが成功しました！
+                    <br>
+                    <form method = "GET" action="{{ route('contact.index') }}">
+                    <input class="btn btn-info" type="submit" value="一覧を見る">                </div>
             </div>
         </div>
     </div>
